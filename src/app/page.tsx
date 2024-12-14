fdcad64e35ae4dd949251c1066cfda3e4623a54b
martinsvarc2 <marianekjohn9@gmail.com>
2024-12-14 12:43:17 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import Timer from '@/components/Timer';
+import Timer from '../components/Timer';
 
 export default function Home() {
   const searchParams = useSearchParams();
