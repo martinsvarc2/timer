0f584231fec1b6fb0185b54735ad15a090e98717
martinsvarc2 <marianekjohn9@gmail.com>
2024-12-15 01:04:14 +0100

    Update Timer.tsx

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
 interface TimerProps {
