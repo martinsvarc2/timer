4564aa55f0b171fbe6be5ba3a79ddf8ec9bf58d5
martinsvarc2 <marianekjohn9@gmail.com>
2024-12-15 10:27:24 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,28 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)'],
+      },
+      keyframes: {
+        heartbeat: {
+          '0%, 100%': { 
+            opacity: '1',
+            transform: 'scale(1)'
+          },
+          '50%': { 
+            opacity: '0.8',
+            transform: 'scale(1.05)'
+          },
+        },
+      },
+      animation: {
+        heartbeat: 'heartbeat 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
+
 export default config
